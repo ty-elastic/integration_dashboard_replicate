26becc53f5c72d1849bff6a38a8ea254c7d18413
Ty Bekiares <tyrone.bekiares@elastic.co>
2023-09-13 15:27:56 -0500

    updates

--- main.py
+++ main.py
@@ -1,6 +1,57 @@
 import requests
 import ndjson
 import sys, getopt
+import json
+
+# def export_dashboards(kibana_server, user, password, admin_space_id, object_ids):
+#     data = {
+#         "objects": [],
+#         "includeReferencesDeep": True
+#     }
+#     for object in object_ids:
+#         data['objects'].append(object)
+
+#     if admin_space_id == 'default':
+#         URL = f"{kibana_server}/api/saved_objects/_export"
+#     else:
+#         URL = f"{kibana_server}/s/{admin_space_id}/api/saved_objects/_export"
+
+#     #print(URL)
+#     resp = requests.post(URL, json=data, auth=(user, password), headers={"kbn-xsrf": "reporting"})
+#     #print(resp.content)
+#     return resp.content
+
+# def retry_dashboards(kibana_server, user, password, viewer_space_id, integration_dashboards, errors, successResults):
+#     retries = []
+#     for error in errors:
+#         retry = {'id': error['id'], 'type': error['type'], 'overwrite': True, 'ignoreMissingReferences': True}
+#         retries.append(retry)
+
+#     # for success in successResults: 
+#     #     retry = {'id': error['id'], 'type': error['type'], 'overwrite': True}
+#     #     retries.append(retry)
+
+#     if viewer_space_id == 'default':
+#         URL = f"{kibana_server}/api/saved_objects/_resolve_import_errors"
+#     else:
+#         URL = f"{kibana_server}/s/{viewer_space_id}/api/saved_objects/_resolve_import_errors"
+#     print(retries)
+#     resp = requests.post(URL, files={"file": ("export.ndjson", integration_dashboards), "retries": (None, json.dumps(retries))}, auth=(user, password), headers={"kbn-xsrf": "reporting"}, params={'compatibilityMode': True, 'createNewCopies': False})   
+#     print(resp)
+
+# def import_dashboards(kibana_server, user, password, viewer_spaces, integration_dashboards):
+#     for space in viewer_spaces:
+#         if space == 'default':
+#             URL = f"{kibana_server}/api/saved_objects/_import"
+#         else:
+#             URL = f"{kibana_server}/s/{space}/api/saved_objects/_import"
+#         #print(URL)
+#         resp = requests.post(URL, files={"file": ("export.ndjson", integration_dashboards)}, auth=(user, password), headers={"kbn-xsrf": "reporting"}, params={'compatibilityMode': True, 'overwrite': True})   
+#         #print(resp.content)
+#         print(f"importing dashboards into space:{space}...")
+#         if resp.json()['success'] == False:
+#             print(f"error importing some objects")
+#             retry_dashboards(kibana_server, user, password, space, integration_dashboards, resp.json()['errors'],resp.json()['successResults'])
 
 def get_integration_dashboards(kibana_server, user, password, admin_space_id):
     data = {
@@ -18,10 +69,11 @@ def get_integration_dashboards(kibana_server, user, password, admin_space_id):
                 if ref['type'] == 'tag':
                     if ref['id'].find('fleet') != -1:
                         if not obj['id'] in integration_object_ids and 'type' in obj and obj['type'] == 'dashboard':
-                            print(obj)
-                            integration_object_ids.append(obj['id'])
+                            #print(obj)
+                            if 'type' in obj:
+                                integration_object_ids.append({'type': obj['type'], 'id': obj['id']})
                         break
-    #print(integration_object_ids)
+    print(integration_object_ids)
     return integration_object_ids
 
 def copy_dashboards(kibana_server, user, password, admin_space_id, object_ids, viewer_space_ids):
@@ -33,8 +85,8 @@ def copy_dashboards(kibana_server, user, password, admin_space_id, object_ids, v
         "createNewCopies": False,
         "spaces": viewer_space_ids
     }
-    for id in object_ids:
-        data['objects'].append({"type": "dashboard", "id":id})
+    for object in object_ids:
+        data['objects'].append(object)
 
     if admin_space_id == 'default':
         URL = f"{kibana_server}/api/spaces/_copy_saved_objects"
@@ -43,6 +95,7 @@ def copy_dashboards(kibana_server, user, password, admin_space_id, object_ids, v
     resp = requests.post(URL, json=data, auth=(user, password), headers={"kbn-xsrf": "reporting"})
     print(resp.content)
     return resp.content
+
 
 def get_viewer_space_ids(kibana_server, user, password, admin_space_id):
     URL = f"{kibana_server}/api/spaces/space"
@@ -58,11 +111,14 @@ def get_viewer_space_ids(kibana_server, user, password, admin_space_id):
 def replicate_integration_dashboards(kibana_server, user, password, admin_space_id, viewer_space_ids=None):
 
     integration_object_ids = get_integration_dashboards(kibana_server, user, password, admin_space_id)
+    print(integration_object_ids)
 
     if viewer_space_ids == None:
         viewer_space_ids = get_viewer_space_ids(kibana_server, user, password, admin_space_id)
 
     copy_dashboards(kibana_server, user, password, admin_space_id, integration_object_ids, viewer_space_ids)
+    #integration_dashboards = export_dashboards(kibana_server, user, password, admin_space_id, integration_object_ids)
+    #import_dashboards(kibana_server, user, password, viewer_space_ids, integration_dashboards)
 
 def main(argv):
     opts, args = getopt.getopt(argv,"s:u:p:a:")
